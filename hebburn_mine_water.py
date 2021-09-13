d3ca2d06c30012085b1dbd286653537a82e95670
W Wei <wgang.wei@gmail.com>
2021-09-13 20:20:07 +0100

    Add plot fun to plot LAeq and LAFmax directly

--- hebburn_mine_water.py
+++ hebburn_mine_water.py
@@ -12,6 +12,7 @@ def to_ymd_hms(time_string):
     s = time_string.split(' ')
     hms = s[-1]
     dmy = s[0].split('/')
+    # dmy = s[0].split('-')
     d = dmy[0]
     m = dmy[1]
     y = dmy[2]
@@ -21,7 +22,8 @@ def to_ymd_hms(time_string):
 
 class Noise():
     def __init__(self,noise_file):
-        self.noise = pd.read_csv(noise_file)
+        print('Load noise data')
+        self.noise = pd.read_csv(noise_file, dtype={'Leq': np.float64, 'Lmax': np.float64})
         datetime_obj = []
         for dt in self.noise['Time']:
             ymd_hms = to_ymd_hms(dt)
@@ -71,10 +73,39 @@ class Noise():
         output = output.transpose()
         output.to_csv('Noise events count.csv')
 
+    def plot_time_history(self):
+        wk1_start_date = self.start_date
+        wk1_end_date = wk1_start_date + pd.Timedelta(5, "days")
+        wk2_start_date = wk1_start_date + pd.Timedelta(7, "days")
+        wk2_end_date = wk2_start_date + pd.Timedelta(5, "days")
+        wk3_start_date = wk2_start_date + pd.Timedelta(7, "days")
+        wk3_end_date = wk3_start_date + pd.Timedelta(5, "days")
+        wk4_start_date = wk3_start_date + pd.Timedelta(7, "days")
+        wk4_end_date = wk4_start_date + pd.Timedelta(5, "days")
+        weeks = [[wk1_start_date, wk1_end_date], [wk2_start_date, wk2_end_date], 
+        [wk3_start_date, wk3_end_date], [wk4_start_date, wk4_end_date]]
+
+        for wk in weeks:
+            start_time = wk[0]
+            end_time = wk[1]
+            df2 = self.noise[(self.noise['Time_obj']>=start_time) & (self.noise['Time_obj']<end_time)]
+            data_len = df2.shape[0]
+            x_tick_id = [0, int(data_len*0.2), int(data_len*0.4), int(data_len*0.6), int(data_len*0.8)]
+            x_tick_values = [df2['Time_obj'].iloc[i].date() for i in x_tick_id]
+            plt.figure()
+            plt.plot(range(data_len), df2['Leq'])
+            plt.plot(range(data_len), df2['Lmax'])
+            plt.grid()
+            plt.ylabel('dB')
+            plt.legend(['LAeq','LAFmax'])
+            plt.xticks(x_tick_id, x_tick_values)
+        plt.show()
+
 
 class Vibration():
     def __init__(self, vib_file):
-        self.vib = pd.read_csv(vib_file)
+        print('Load vibration data')
+        self.vib = pd.read_csv(vib_file, dtype={'V':np.float64})
         datetime_obj = []
         for dt in self.vib['Time']:
             ymd_hms = to_ymd_hms(dt)
@@ -113,14 +144,15 @@ class Vibration():
 
 
 def main():
-    os.chdir(r"G:\Shared drives\8000 - 8999\8500 - 8599\8527 Hebburn Mine Water noise and vib monitoring\Reports\Monitoring report\20210719-20210813")
-    file_noise = "interval_8527_6147998_2021-07-19-0000-2021-08-13-2359_noise.csv"
-    file_vibration = "interval_8527_6147998_2021-07-19-0000-2021-08-13-2359_vibration.csv"
+    os.chdir(r"G:\Shared drives\8000 - 8999\8500 - 8599\8527 Hebburn Mine Water noise and vib monitoring\Reports\Monitoring report\20210816-20210910")
+    file_noise = "interval_8527_6291309_2021-08-16-0000-2021-09-10-2359_noise.csv"
+    file_vibration = "interval_8527_6291309_2021-08-16-0000-2021-09-10-2359_vibration2.csv"
     noise_obj = Noise(file_noise)
-    noise_obj.count_events()
-    noise_obj.calc_LAeq0800_1800()
-    vib_obj = Vibration(file_vibration)
-    vib_obj.count_events()
+    # noise_obj.count_events()
+    # noise_obj.calc_LAeq0800_1800()
+    noise_obj.plot_time_history()
+    # vib_obj = Vibration(file_vibration)
+    # vib_obj.count_events()
 
 
 if __name__=="__main__":
